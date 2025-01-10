2ece93d7e14ff67d981a9f870c1e9c2351b67027
BlackRain <masaraksh86@mail.ru>
2025-01-10 15:13:46 +0700

    Исправлен метод "MultiThreadedDownloaderLib.Utils.SplitContentToChunks()"

--- MultiThreadedDownloaderLib/Utils.cs
+++ MultiThreadedDownloaderLib/Utils.cs
@@ -36,6 +36,7 @@ namespace MultiThreadedDownloaderLib
 		internal static IEnumerable<Tuple<long, long>> SplitContentToChunks(
 			long contentLength, long rangeFrom, long rangeTo, int chunkCount)
 		{
+			if (rangeTo < 0L) { rangeTo = contentLength; }
 			if (contentLength <= 0L || rangeTo < rangeFrom)
 			{
 				yield return new Tuple<long, long>(0L, -1L);
