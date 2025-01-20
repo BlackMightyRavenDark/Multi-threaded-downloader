d398e995f6e7004dbb6bd6621836d64eda63f682
BlackRain <masaraksh86@mail.ru>
2025-01-20 12:22:23 +0700

    [GuiTest]: Изменены заголовки HTTP-запроса по-умолчанию

--- MultiThreadedDownloaderLib.GuiTest/Form1.cs
+++ MultiThreadedDownloaderLib.GuiTest/Form1.cs
@@ -32,9 +32,10 @@ namespace MultiThreadedDownloaderLib.GuiTest
 
 			headerCollection = new NameValueCollection()
 			{
-				{ "User-Agent", "Mozilla/Firefoxxx 66.6" },
+				{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0" },
 				{ "Accept", "*/*" },
-				{ "Range", "0-" }
+				{ "Accept-Language", "en-US" },
+				{ "Accept-Encoding", "gzip, deflate, br" }
 			};
 		}
 
