a2b329d39e571fb429713c644f66cc7d15a1107e
BlackRain <masaraksh86@mail.ru>
2025-03-27 15:12:55 +0700

    Рефакторинг в классе "StreamAppender"

--- MultiThreadedDownloaderLib/src/StreamAppender.cs
+++ MultiThreadedDownloaderLib/src/StreamAppender.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading;
 
 namespace MultiThreadedDownloaderLib
@@ -13,81 +12,79 @@ namespace MultiThreadedDownloaderLib
 		public delegate void StreamAppendFinishedDelegate(long sourcePosition, long sourceLength,
 			long destinationPosition, long destinationLength);
 
-		public static bool Append(Stream streamFrom, Stream streamTo,
+		public static bool Append(Stream inputStream, Stream outputStream,
 			StreamAppendStartedDelegate streamAppendStarted,
 			StreamAppendProgressDelegate streamAppendProgress,
 			StreamAppendFinishedDelegate streamAppendFinished,
 			CancellationToken cancellationToken,
-			int updateIntervalMilliseconds, int bufferSize = 4096)
+			long updateIntervalMilliseconds, int bufferSize = 4096)
 		{
-			streamAppendStarted?.Invoke(streamFrom.Position, streamFrom.Length,
-				streamTo.Position, streamTo.Length);
-
-			if (updateIntervalMilliseconds <= 0 || bufferSize <= 0 ||
-				streamFrom == null || streamTo == null ||
-				streamFrom.Position != 0L || streamTo.Position != streamTo.Length)
+			if (bufferSize <= 0 ||
+				inputStream == null || outputStream == null ||
+				inputStream.Position != 0L || outputStream.Position != outputStream.Length)
 			{
 				return false;
 			}
 
-			long size = streamTo.Length;
+			streamAppendStarted?.Invoke(inputStream.Position, inputStream.Length,
+				outputStream.Position, outputStream.Length);
+
+			long inputStreamLength = inputStream.Length;
+			long outputStreamInitialLength = outputStream.Length;
 			byte[] buffer = new byte[bufferSize];
 
-			int lastTime = streamAppendProgress != null ? Environment.TickCount : 0;
+			System.Diagnostics.Stopwatch stopwatch =
+				streamAppendProgress != null && updateIntervalMilliseconds > 0L ?
+				new System.Diagnostics.Stopwatch() : null;
 			do
 			{
-				int bytesRead = streamFrom.Read(buffer, 0, buffer.Length);
-				if (bytesRead <= 0)
-				{
-					break;
-				}
-				streamTo.Write(buffer, 0, bytesRead);
+				int bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+				if (bytesRead <= 0) { break; }
+				outputStream.Write(buffer, 0, bytesRead);
 
-				if (streamAppendProgress != null)
+				if (stopwatch != null && stopwatch.ElapsedMilliseconds >= updateIntervalMilliseconds)
 				{
-					int currentTime = Environment.TickCount;
-					if (currentTime - lastTime >= updateIntervalMilliseconds)
-					{
-						streamAppendProgress.Invoke(
-							streamFrom.Position, streamFrom.Length,
-							streamTo.Position, streamTo.Length);
-						lastTime = currentTime;
-					}
+					streamAppendProgress.Invoke(
+						inputStream.Position, inputStreamLength,
+						outputStream.Position, outputStream.Length);
+					stopwatch.Reset();
 				}
 			} while (!cancellationToken.IsCancellationRequested);
+			streamAppendProgress?.Invoke(inputStream.Position, inputStreamLength,
+				outputStream.Position, outputStream.Length);
 
-			streamAppendFinished?.Invoke(streamFrom.Position, streamFrom.Length,
-				streamTo.Position, streamTo.Length);
+			streamAppendFinished?.Invoke(inputStream.Position, inputStreamLength,
+				outputStream.Position, outputStream.Length);
 
-			return streamTo.Length == size + streamFrom.Length;
+			return outputStream.Length == outputStreamInitialLength + inputStreamLength;
 		}
 
-		public static bool Append(Stream streamFrom, Stream streamTo,
+		public static bool Append(Stream inputStream, Stream outputStream,
 			StreamAppendStartedDelegate streamAppendStarted,
 			StreamAppendProgressDelegate streamAppendProgress,
 			StreamAppendFinishedDelegate streamAppendFinished,
 			CancellationToken cancellationToken,
-			int updateIntervalMilliseconds = 100)
+			long updateIntervalMilliseconds = 100L)
 		{
-			return Append(streamFrom, streamTo,
+			return Append(inputStream, outputStream,
 				streamAppendStarted, streamAppendProgress, streamAppendFinished,
 				cancellationToken, updateIntervalMilliseconds, 4096);
 		}
 
-		public static bool Append(Stream streamFrom, Stream streamTo,
+		public static bool Append(Stream inputStream, Stream outputStream,
 			StreamAppendStartedDelegate streamAppendStarted,
 			StreamAppendProgressDelegate streamAppendProgress,
 			StreamAppendFinishedDelegate streamAppendFinished,
-			int updateIntervalMilliseconds = 100)
+			long updateIntervalMilliseconds = 100L)
 		{
-			return Append(streamFrom, streamTo,
+			return Append(inputStream, outputStream,
 				streamAppendStarted, streamAppendProgress, streamAppendFinished,
 				default, updateIntervalMilliseconds);
 		}
 
-		public static bool Append(Stream streamFrom, Stream streamTo)
+		public static bool Append(Stream inputStream, Stream outputStream)
 		{
-			return Append(streamFrom, streamTo, null, null, null);
+			return Append(inputStream, outputStream, null, null, null);
 		}
 	}
 }
