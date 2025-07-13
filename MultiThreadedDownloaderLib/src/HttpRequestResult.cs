7c478d375459c02294d31038fb9a7be4fef0c389
BlackRain <masaraksh86@mail.ru>
2025-07-13 17:50:22 +0700

    Добавлено свойство "HttpRequestResult.HasReadableContent"

--- MultiThreadedDownloaderLib/src/HttpRequestResult.cs
+++ MultiThreadedDownloaderLib/src/HttpRequestResult.cs
@@ -13,6 +13,7 @@ namespace MultiThreadedDownloaderLib
 		public bool HasErrorMessage => HasErrorMessageText();
 		public HttpWebResponse HttpWebResponse { get; private set; }
 		public bool IsExceptionRaised { get; }
+		public bool HasReadableContent => WebContent?.Data != null && WebContent.Data.CanRead && WebContent.Data.Length > 0L;
 		public WebContent WebContent { get; private set; }
 
 		public HttpRequestResult(int errorCode, string errorMessage,
